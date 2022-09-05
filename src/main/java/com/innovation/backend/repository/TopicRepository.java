0f24f4dd33f67213c5acbc997c4549f695b7bfc4
송민진 <m_6595@naver.com>
2022-09-06 01:25:57 +0900

    주제 불러오기 기능 완료

--- src/main/java/com/innovation/backend/repository/TopicRepository.java
+++ src/main/java/com/innovation/backend/repository/TopicRepository.java
@@ -3,5 +3,7 @@ package com.innovation.backend.repository;
 import com.innovation.backend.entity.Topic;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TopicRepository extends JpaRepository<Topic,Long> {
 }

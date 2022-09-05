0c151d726696e585fa68c1a0617a3d95e7191e58
Hyemm <hmlee5938@gmail.com>
2022-09-05 13:02:58 +0900

    로그인 연동

--- src/main/java/com/innovation/backend/controller/LikesController.java
+++ src/main/java/com/innovation/backend/controller/LikesController.java
@@ -2,8 +2,10 @@ package com.innovation.backend.controller;
 
 import com.innovation.backend.dto.response.ResponseDto;
 import com.innovation.backend.entity.Member;
+import com.innovation.backend.security.user.UserDetailsImpl;
 import com.innovation.backend.service.LikesService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,13 +18,13 @@ public class LikesController {
     private final LikesService likesService;
 
     @RequestMapping(value = "/{interviewId}/like", method = RequestMethod.POST)
-    public ResponseDto<?> createLike(@PathVariable Long interviewId/*, @AuthenticationPrincipal UserDetailsImpl userDetails*/){
-        Member member = null;
+    public ResponseDto<?> createLike(@PathVariable Long interviewId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Member member = userDetails.getMember();
         return likesService.createLike(interviewId, member);
     }
     @RequestMapping(value = "/{interviewId}/like", method = RequestMethod.DELETE)
-    public ResponseDto<?> deleteLike(@PathVariable Long interviewId/*, @AuthenticationPrincipal UserDetailsImpl userDetails*/){
-        Member member = null;
+    public ResponseDto<?> deleteLike(@PathVariable Long interviewId, @AuthenticationPrincipal UserDetailsImpl userDetails){
+        Member member = userDetails.getMember();
         return likesService.deleteLike(interviewId, member);
     }
 }

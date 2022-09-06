1f90ab5c6f6746a93eda71e496ba5ad71f153d25
kky7 <kykim708@gmail.com>
2022-09-06 15:07:24 +0900

    cors 추가

--- src/main/java/com/innovation/backend/security/config/WebSecurityConfig.java
+++ src/main/java/com/innovation/backend/security/config/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package com.innovation.backend.security.config;
 
 import com.innovation.backend.jwt.filter.JwtAuthFilter;
 import com.innovation.backend.jwt.util.JwtUtil;
+import com.innovation.backend.jwt.util.TokenProperties;
 import com.innovation.backend.security.user.UserDetailsServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @EnableWebSecurity
 @Configuration
@@ -37,17 +41,37 @@ public class WebSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        http.cors();
+        http.cors().configurationSource(corsConfigurationSource());
         http.csrf().disable()
                 .addFilterBefore(new JwtAuthFilter(jwtUtil,userDetailsService),UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeRequests((authz)->authz
-                        .antMatchers("/auth/**").authenticated()
-                        .anyRequest().permitAll());
+                .antMatchers("/auth/**").authenticated()
+                .anyRequest().permitAll());
 
         return http.build();
+    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        //허용할 url 설정
+        configuration.addAllowedOrigin("http://localhost:3000");
+        //허용할 헤더 설정
+        configuration.addAllowedHeader("*");
+        //허용할 http method
+        configuration.addAllowedMethod("*");
+        // 허용할 헤더
+        configuration.addExposedHeader(TokenProperties.AUTH_HEADER);
+        configuration.addExposedHeader(TokenProperties.REFRESH_HEADER);
+        //사용자 자격 증명이 지원되는지 여부
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+
     }
 
 }

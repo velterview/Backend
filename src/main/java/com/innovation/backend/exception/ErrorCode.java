82c1c69da05b7be86df10dd134c97f9a4108d646
kky7 <kykim708@gmail.com>
2022-09-06 10:51:18 +0900

    중복코드 삭제

--- src/main/java/com/innovation/backend/exception/ErrorCode.java
+++ src/main/java/com/innovation/backend/exception/ErrorCode.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-@JsonFormat(shape = JsonFormat.Shape.OBJECT)
 @AllArgsConstructor
 @Getter
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)

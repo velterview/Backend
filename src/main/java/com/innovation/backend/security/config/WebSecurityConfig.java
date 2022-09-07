5a9708958e8398b40fdf43dda6a7ccec112c6bb0
kky7 <kykim708@gmail.com>
2022-09-07 20:23:58 +0900

    Update WebSecurityConfig.java

--- src/main/java/com/innovation/backend/security/config/WebSecurityConfig.java
+++ src/main/java/com/innovation/backend/security/config/WebSecurityConfig.java
@@ -62,7 +62,7 @@ public class WebSecurityConfig {
         configuration.addAllowedHeader("*");
         //허용할 http method
         configuration.addAllowedMethod("*");
-        // 라이언트가 접근 할 수 있는 서버 응답 헤더
+        //클라이언트가 접근 할 수 있는 서버 응답 헤더
         configuration.addExposedHeader(TokenProperties.AUTH_HEADER);
         configuration.addExposedHeader(TokenProperties.REFRESH_HEADER);
         //사용자 자격 증명이 지원되는지 여부
